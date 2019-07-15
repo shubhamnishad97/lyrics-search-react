1f3e6223d4e20ba88229797c4fb29108c1983f52
Shubham Nishad <shubhamnishad25@gmail.com>
2019-07-15 17:25:22 +0530

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import { BrowserRouter, Route, Switch }  from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  	<BrowserRouter>
+	    <div className="App">
+	     <Navbar/>
+		     <div className="container">
+		     <Switch>
+		     	<Route exact path="/" component={Index}/>
+		     </Switch>
+		     </div>
+	    </div>
+    </BrowserRouter>
   );
 }
 
