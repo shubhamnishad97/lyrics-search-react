f1604ccb5f511276d2303bd2d68a6ac285b09225
Shubham Nishad <shubhamnishad25@gmail.com>
2019-07-16 17:33:29 +0530

    add Lyrics.js
    made the Lyrics component to dispplay lyrics of the selected/searched song.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
+import Lyrics from "./components/tracks/Lyrics";
+
 import { BrowserRouter, Route, Switch }  from 'react-router-dom';
 
-import { Provider } from './context'
+import { Provider } from './context';
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
 						<div className="container">
 						<Switch>
 							<Route exact path="/" component={Index}/>
+							<Route exact path="/lyrics/track/:id" component={Lyrics} />
 						</Switch>
 						</div>
 				</div>
